22914c821c4783b45d4a2c55e40bdca0adf213a1
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-23 09:40:09 -0500

    initial commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      taskOne: 'Annoy Cat',
+      taskTwo: 'Take out Dishes',
+      taskThree: 'Do laundry',
+    }
+  });
+});
